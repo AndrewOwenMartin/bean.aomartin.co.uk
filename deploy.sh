ccc97047d10b03b67d65d9f2c30aa65082744edb
Andrew Owen Martin <andrew@aomartin.co.uk>
2026-04-06 15:44:09 +0100

    Restart nginx after deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ echo 'Deleting old /assets'
 rm -r /var/www/bean.aomartin.co.uk/assets/
 echo 'Moving dist/assets to /assets'
 mv -v /var/www/bean.aomartin.co.uk/html/assets/ /var/www/bean.aomartin.co.uk/
+echo 'Reloading nginx'
+systemctl reload nginx
 
 # old
 # cp -v html/index.html /var/www/bean.aomartin.co.uk/html
